742fca489763d7af212800f24a8c9d1263195ac8
Pavel <pavel.stepanov@omega-r.com>
2017-07-07 15:58:14 +0300

    add test example

--- app/src/main/java/com/omega_r/omegarecyclerview/MainActivity.java
+++ app/src/main/java/com/omega_r/omegarecyclerview/MainActivity.java
@@ -2,12 +2,31 @@ package com.omega_r.omegarecyclerview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.omega_r.libs.omegarecyclerview.OmegaRecyclerView;
+import com.omega_r.omegarecyclerview.adapter.ContactsAdapter;
+import com.omega_r.omegarecyclerview.model.Contact;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+    ArrayList<Contact> mContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OmegaRecyclerView omegaRecyclerView = (OmegaRecyclerView) findViewById(R.id.recycler_view_contacts);
+
+        mContacts = Contact.createContactsList(20);
+        ContactsAdapter adapter = new ContactsAdapter(this, mContacts);
+        omegaRecyclerView.setAdapter(adapter);
+        omegaRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mContacts.add(20, new Contact(null, false));
+        adapter.notifyItemInserted(20);
     }
 }
--- omegarecyclerviewlibs/src/main/java/com/omega_r/libs/omegarecyclerview/OmegaRecyclerView.java
+++ omegarecyclerviewlibs/src/main/java/com/omega_r/libs/omegarecyclerview/OmegaRecyclerView.java
@@ -26,14 +26,17 @@ public class OmegaRecyclerView extends RecyclerView {
 
     public OmegaRecyclerView(Context context) {
         super(context);
+        init(context, null, 0);
     }
 
     public OmegaRecyclerView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs, 0);
     }
 
     public OmegaRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        init(context, attrs, defStyle);
     }
 
     private void init(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
