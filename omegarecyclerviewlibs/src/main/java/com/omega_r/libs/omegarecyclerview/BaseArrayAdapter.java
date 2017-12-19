b363709c91a5fb11a595fd938eb1bf735b0be0f8
roman_tcaregorodtcev <roman.tc@omega-r.com>
2017-12-19 14:27:47 +0300

    merge develop

--- omegarecyclerviewlibs/src/main/java/com/omega_r/libs/omegarecyclerview/BaseArrayAdapter.java
+++ omegarecyclerviewlibs/src/main/java/com/omega_r/libs/omegarecyclerview/BaseArrayAdapter.java
@@ -8,19 +8,11 @@ import java.util.Arrays;
 public abstract class BaseArrayAdapter<M, VH extends RecyclerView.ViewHolder> extends OmegaRecyclerView.Adapter<VH> {
 
     private M[] mArray;
-    private RecyclerView mRecyclerView;
 
     public BaseArrayAdapter(M[] array) {
         mArray = array;
     }
 
-    @Override
-    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-        super.onAttachedToRecyclerView(recyclerView);
-        mRecyclerView = recyclerView;
-    }
-
-    @Override
     public int getItemCount() {
         return mArray.length;
     }
@@ -31,13 +23,13 @@ public abstract class BaseArrayAdapter<M, VH extends RecyclerView.ViewHolder> ex
 
     public void set(M... array) {
         mArray = array;
-        notifyDataSetChangedSafe();
+        tryNotifyDataSetChanged();
     }
 
     public void add(M... array) {
         int length = mArray.length;
         mArray = concat(mArray, array);
-        notifyItemRangeInsertedSafe(length, array.length);
+        tryNotifyItemRangeInserted(length, array.length);
     }
 
     private static <T> T[] concat(T[] first, T[] second) {
@@ -45,33 +37,4 @@ public abstract class BaseArrayAdapter<M, VH extends RecyclerView.ViewHolder> ex
         System.arraycopy(second, 0, result, first.length, second.length);
         return result;
     }
-
-
-    protected void notifyDataSetChangedSafe() {
-        if (!mRecyclerView.isComputingLayout()) {
-            notifyDataSetChanged();
-        } else {
-            mRecyclerView.post(new Runnable() {
-                @Override
-                public void run() {
-                    notifyDataSetChangedSafe();
-                }
-            });
-        }
-    }
-
-
-    protected void notifyItemRangeInsertedSafe(final int positionStart, final int itemCount) {
-        if (!mRecyclerView.isComputingLayout()) {
-            notifyItemRangeInserted(positionStart, itemCount);
-        } else {
-            mRecyclerView.post(new Runnable() {
-                @Override
-                public void run() {
-                    notifyItemRangeInserted(positionStart, itemCount);
-                }
-            });
-        }
-    }
-
 }
