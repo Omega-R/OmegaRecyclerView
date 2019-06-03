8929028720b47ce63ae304fe45b1485c3aa32bcd
Anton Knyazev <anton.knyazev@omega-r.com>
2019-06-03 17:01:38 +0300

    Add support GridLayoutManager for itemSpace

--- omegarecyclerviewlibs/src/main/java/com/omega_r/libs/omegarecyclerview/item_decoration/SpaceItemDecoration.java
+++ omegarecyclerviewlibs/src/main/java/com/omega_r/libs/omegarecyclerview/item_decoration/SpaceItemDecoration.java
@@ -1,10 +1,12 @@
 package com.omega_r.libs.omegarecyclerview.item_decoration;
 
 import android.graphics.Rect;
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.omega_r.libs.omegarecyclerview.item_decoration.decoration_helpers.DividerDecorationHelper;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SpaceItemDecoration extends BaseItemDecoration {
 
@@ -18,8 +20,16 @@ public class SpaceItemDecoration extends BaseItemDecoration {
     @Override
     void getItemOffset(@NonNull Rect outRect, @NonNull RecyclerView parent,
                        @NonNull DividerDecorationHelper helper, int position, int itemCount) {
-        if (isShowBeginDivider() && position < 1 || position >= 1) helper.setStart(outRect, space);
-        if (isShowEndDivider() && position == itemCount - 1) helper.setEnd(outRect, space);
+        int countBeginEndPositions = getCountBeginEndPositions(parent);
+        if (isShowBeginDivider() || countBeginEndPositions <= position) helper.setStart(outRect, space);
+        if (isShowEndDivider() && position == itemCount - countBeginEndPositions) helper.setEnd(outRect, space);
+    }
+
+    private int getCountBeginEndPositions(RecyclerView recyclerView) {
+        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            return ((GridLayoutManager) layoutManager).getSpanCount();
+        } else return 1;
     }
 
 }
